a5ed65fa176a21a5d5ed9db069d57789bf724e82
victor-guardia <vanguarda1984@gmail.com>
2022-09-13 17:30:03 -0300

    multiplicacion de dos variables numericas

---
+++ module1.py
@@ -0,0 +1,22 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      vangu
+#
+# Created:     09/09/2022
+# Copyright:   (c) vangu 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+   alfajores= 50
+   cajas= 200
+   costo= alfajores * cajas
+   print(costo)
+
+
+if __name__ == '__main__':
+    main()
+
+
