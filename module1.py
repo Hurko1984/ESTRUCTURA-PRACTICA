30d6a4937604fc7880bcf20b47fc09dab6cf6cb4
victor-guardia <vanguarda1984@gmail.com>
2022-10-08 07:32:51 -0300

    ejercicio 3 mail valido

--- module1.py
+++ module1.py
@@ -11,35 +11,21 @@
 
 def main():
 
-
-    num1 = int(input("Digite un numero: "))
-    num2 = int(input("digite un segundo numero"))
-    print(" 1. sumar, 2. restar, 3. multiplicar")
-    opcion = int(input("ingrese una opcion"))
-
-    while(opcion == 0 or  opcion >3):
-
-        print(" 1. sumar, 2.restar, 3. multiplicar")
-
-        opcion = int(input("ingrese una opcion: "))
+    palabra = input("ingrese su email: ").lower()
+    arroba = "probador"
+    for caracter in palabra:
+        arroba = "mail valido"
+        if caracter == "@":
+            break
+        else:
+            arroba = "mail invalido"
 
 
-    if opcion == (1):
-         resu = num1 + num2
-
-    elif opcion == (2):
-            resu = num1 - num2
-    else:
-            resu =num1 * num2
-
-
-    print(f' El resultado es: {resu}')
-
-
+    print(arroba)
 main()
 
 
-
+#print(f'El nombre del usuario es {nombre}')
 
 
 
